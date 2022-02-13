9ab0797477149e4915ab81b9a4afe2e6be4ee63f
Mihai Muntean <mihai@nyble.co.uk>
2022-02-14 00:20:13 +0200

    first commit

---
+++ Nyble.Web.Co2/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Nyble.Web.Co2.Controllers
+{
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public async Task<IActionResult> Get()
+        {
+            return Content("pong");
+        }
+    }
+}
